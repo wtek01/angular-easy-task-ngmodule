6db719da0c3849a077deb54cf71096c5d6b1c125
Moh Med <gmelk02@gmail.com>
2024-08-21 20:58:55 +0200

    clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
